f5f1cfcd0162598d9bc89eee9d883e05b628abc0
Xiangmin Liu <635750556@qq.com>
2018-08-22 23:28:27 +0800

    fix error layout for styleci

--- Bootstrap/Runtime/assert/layout.php
+++ Bootstrap/Runtime/assert/layout.php
@@ -2,5 +2,5 @@
         
 <div id="content">
     <p id="title"><?php echo $title; ?></p>
-    <p id="sub-title"><?php echo $message;?></p>
+    <p id="sub-title"><?php echo $message; ?></p>
 </div>
